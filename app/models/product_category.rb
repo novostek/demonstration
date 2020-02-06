7b1bfe5dffb58a3bc76cae2410224adfe5c6d71b
Gabriel <gabrielvash@gmail.com>
2020-02-06 00:18:41 -0300

    wo-4 product_category

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,2 +1,13 @@
 class ProductCategory < ApplicationRecord
+
+
+  before_save :slug_namespace
+
+
+  #Método que executa slugfy para o namespace caso seja vazio
+  def slug_namespace
+    if self.namespace.blank?
+      self.namespace = self.name.parameterize
+    end
+  end
 end

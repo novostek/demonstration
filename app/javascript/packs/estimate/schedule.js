e35b6734658ad238aca2209e5f078b410627b41d
João Victor <jovictor.miguel@gmail.com>
2020-02-19 11:06:25 -0300

    inicializado o calendário

--- app/javascript/packs/estimate/schedule.js
+++ app/javascript/packs/estimate/schedule.js
@@ -1,10 +1,34 @@
 import { Calendar } from "@fullcalendar/core";
-import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction";
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import $ from 'jquery'
 
 document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('fc-external-drag');
+	var calendarEl = document.getElementById('calendar');
+	var containerEl = document.getElementById('external-events');
+	var checkbox = document.getElementById('drop-remove');
+	
+	var colorData;
+  $('#external-events .fc-event').each(function () {
+    // Different colors for events
+    $(this).css({ 'backgroundColor': $(this).data('color'), 'borderColor': $(this).data('color') });
+  });
+  var colorData;
+  $('#external-events .fc-event').mousemove(function () {
+  	colorData = $(this).data('color');
+  })
+  // Draggable event init
+	new Draggable(containerEl, {
+  	itemSelector: '.fc-event',
+  	eventData: function (eventEl) {
+  		return {
+				title: eventEl.innerText,
+				color: colorData
+  		};
+  	}
+	});
+	
 
   var calendar = new Calendar(calendarEl, {
   	header: {
@@ -13,62 +37,11 @@ document.addEventListener('DOMContentLoaded', function() {
   		right: "dayGridMonth,timeGridWeek,timeGridDay"
   	},
   	editable: true,
-  	plugins: [timeGridPlugin, dayGridPlugin, interactionPlugin],
+  	plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
     droppable: true, // this allows things to be dropped onto the calendar
-    defaultDate: '2020-01-01',
+    defaultDate: new Date(),
     defaultView: 'timeGridWeek',
-    events: [
-    {
-    	title: 'All Day Event',
-    	start: '2020-01-01',
-    	color: '#009688'
-    },
-    {
-    	title: 'Long Event',
-    	start: '2020-01-07',
-    	end: '2020-01-10',
-    	color: '#4CAF50'
-    },
-    {
-    	id: 999,
-    	title: 'Meeting',
-    	start: '2020-01-09T16:00:00',
-    	color: '#00bcd4'
-    },
-    {
-    	id: 999,
-    	title: 'Happy Hour',
-    	start: '2020-01-16T16:00:00',
-    	color: '#3f51b5'
-    },
-    {
-    	title: 'Conference Meeting',
-    	start: '2020-01-11',
-    	end: '2020-01-13',
-    	color: '#e51c23'
-    },
-    {
-    	title: 'Meeting',
-    	start: '2020-01-12T10:30:00',
-    	end: '2020-01-12T12:30:00',
-    	color: '#00bcd4'
-    },
-    {
-    	title: 'Dinner',
-    	start: '2020-01-12T20:00:00',
-    	color: '#4a148c'
-    },
-    {
-    	title: 'Birthday Party',
-    	start: '2020-01-13T07:00:00',
-    	color: '#ff5722'
-    },
-    {
-    	title: 'Click for Google',
-    	url: 'http://google.com/',
-    	start: '2020-01-28',
-    }
-    ],
+    events: [],
     drop: function (info) {
       // is the "remove after drop" checkbox checked?
       if (checkbox.checked) {
@@ -78,7 +51,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  calendar.render();
+	calendar.render();
+	
+	
+  
 })
 
 // $(document).ready(function () {

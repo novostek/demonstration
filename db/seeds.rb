da87e77f7ac697c5d93e24daebd513a8264118d0
Gabriel Santos <gabrielvash@gmail.com>
2020-02-21 14:20:09 -0300

    plutus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Setting.create!([
   {namespace: "company_name", value: {"value"=>"Woffice"}},
-  {namespace: "mail_to_customer", value: {"value"=>{"send_from"=>"Customer", "template_id"=>"d-209a08276e804f778e57cefb058b52d1", "objects"=>[{"class"=>"Customer", "fields"=>[{"field"=>"name", "sendgridkey"=>"customer_name"}, {"field"=>"category", "sendgridkey"=>"customer_category"}, {"field"=>"since", "sendgridkey"=>"customer_since"}, {"field"=>"code", "sendgridkey"=>"customer_code"}]}]}}},
-  {namespace: "send_grid_key", value: {"value"=>"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"}}
+  {namespace: "send_grid_key", value: {"value"=>"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"}},
+  {namespace: "square_key", value: {"value"=>"EAAAEH83t1qbAPHypGll3SImNboOkHzhbVZn2XmOIq21ItYy8BXWGrRmXjRUgTaG"}},
+  {namespace: "square_app_id", value: {"value"=>"sandbox-sq0idb-g75z4FJpMVE0s9qUn7GTmQ"}},
+  {namespace: "mail_to_customer", value: {"value"=>{"send_from"=>"Customer", "template_id"=>"d-209a08276e804f778e57cefb058b52d1", "objects"=>[{"class"=>"Customer", "fields"=>[{"field"=>"nome_categoria", "sendgridkey"=>"metodo"}, {"field"=>"notes", "sendgridkey"=>"notes"}]}]}}}
 ])

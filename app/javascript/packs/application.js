a9b8534c4a3dade4d792b5b378d10c887b41207c
João Victor <jovictor.miguel@gmail.com>
2020-02-27 14:38:08 -0300

    realizado correções no measurements e no carregamento dos js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 require("@fullcalendar/core/main.css")
 require('@fullcalendar/daygrid/main.css')
 require('@fullcalendar/timegrid/main.css')
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

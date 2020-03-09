d61a445110b2db20e0cee3aa782dff622329f578
Gabriel <gabrielvash@gmail.com>
2020-03-09 19:53:42 -0300

    criação de usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :profile_users

8dacfc82712b55114bc6841d8b83820b23afb72c
Gabriel <gabrielvash@gmail.com>
2020-03-26 22:38:18 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,14 @@
 Setting.create!([
   {namespace: "company_name", value: {"value"=>"Woffice"}},
-  {namespace: "take_photo", value: {"value"=>true}},
-  {namespace: "order_need_sign", value: {"value"=>true}},
   {namespace: "client_end_order", value: {"value"=>true}},
   {namespace: "send_grid_key", value: {"value"=>"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"}},
   {namespace: "square_key", value: {"value"=>"EAAAEH83t1qbAPHypGll3SImNboOkHzhbVZn2XmOIq21ItYy8BXWGrRmXjRUgTaG"}},
   {namespace: "square_app_id", value: {"value"=>"sandbox-sq0idb-g75z4FJpMVE0s9qUn7GTmQ"}},
-  {namespace: "mail_to_customer", value: {"value"=>{"send_from"=>"Customer", "template_id"=>"d-209a08276e804f778e57cefb058b52d1", "objects"=>[{"class"=>"Customer", "fields"=>[{"field"=>"nome_categoria", "sendgridkey"=>"metodo"}, {"field"=>"notes", "sendgridkey"=>"notes"}]}]}}}
+  {namespace: "mail_to_customer", value: {"value"=>{"send_from"=>"Customer", "template_id"=>"d-209a08276e804f778e57cefb058b52d1", "objects"=>[{"class"=>"Customer", "fields"=>[{"field"=>"nome_categoria", "sendgridkey"=>"metodo"}, {"field"=>"notes", "sendgridkey"=>"notes"}]}]}}},
+  {namespace: "take_photo", value: {"value"=>false}},
+  {namespace: "order_need_sign", value: {"value"=>false}},
+  {namespace: "sign_order_mail_text", value: {"value"=>"Assina ae"}},
+  {namespace: "send_square_mail_text", value: {"value"=>"Pay Text example"}},
+  {namespace: "send_invoice_mail_text", value: {"value"=>"Text example"}},
+  {namespace: "send_document_mail_text", value: {"value"=>"Text example"}}
 ])
-
-10.times do
-  Customer.create(
-    name: Faker::Name.name,
-    birthdate: Faker::Date.birthday(min_age: 18, max_age: 65),
-    category: Faker::Name.category,
-    code: Faker::IDNumber.brazilian_citizen_number,
-    since: Faker::Date.between(from: 2.years.ago, to: Date.today),
-    document_id: Faker::IDNumber.valid
-  )
-end
-
-10.times do
-  Worker.create(
-    name: Faker::Name.name,
-    categories: Faker::Name.worker_category,
-    document_id: Faker::IDNumber.valid
-  )
-end

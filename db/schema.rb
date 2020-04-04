0d0edd626a0905438afd4e9897559028253002f4
João Victor <jovictor.miguel@gmail.com>
2020-04-04 18:31:47 -0300

    refatoramento/wo-127

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_002356) do
+ActiveRecord::Schema.define(version: 2020_04_04_203822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_002356) do
 
   create_table "estimates", force: :cascade do |t|
     t.string "code", null: false
-    t.string "title", null: false
+    t.string "title"
     t.bigint "sales_person_id"
     t.string "status", null: false
     t.text "description", null: false
@@ -301,7 +301,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_002356) do
     t.string "bpm_purchase"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "supplier_id", null: false
+    t.bigint "supplier_id"
     t.bigint "calculation_formula_id", null: false
     t.text "photo"
     t.index ["calculation_formula_id"], name: "index_products_on_calculation_formula_id"

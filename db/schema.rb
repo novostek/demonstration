08bebf1382a28ba309e0c5ab1a3d1ee9a90e9896
Lucas Cunha <lucascunha.dev@gmail.com>
2020-05-14 11:05:57 -0300

    fix commits sprint 15

--- db/schema.rb
+++ db/schema.rb
@@ -128,7 +128,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_175858) do
 
   create_table "labor_costs", force: :cascade do |t|
     t.bigint "worker_id", null: false
-    t.bigint "schedule_id"
+    t.bigint "schedule_id", null: false
     t.date "date"
     t.decimal "value"
     t.string "status"

b50c6d8b2609c3436fdc721b9b2ffc269a8e7e57
Gabriel <gabrielvash@gmail.com>
2020-06-02 20:09:43 -0300

    correcao migrate origin_id transaction

--- db/migrate/20200308192205_add_fields_to_transaction.rb
+++ db/migrate/20200308192205_add_fields_to_transaction.rb
@@ -1,6 +1,6 @@
 class AddFieldsToTransaction < ActiveRecord::Migration[6.0]
   def change
-    add_column :transactions, :origin_id, :integer
+    add_column :transactions, :origin_id, type: :uuid
     add_column :transactions, :payment_method, :string
   end
 end

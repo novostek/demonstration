b52dc54d5ab11a1ad7199dfacc306310f9768554
João Victor <jovictor.miguel@gmail.com>
2020-06-02 22:21:06 -0300

    fix: corrige erro ao calcular com uuid

--- app/controllers/calculation_formulas_controller.rb
+++ app/controllers/calculation_formulas_controller.rb
@@ -54,7 +54,7 @@ class CalculationFormulasController < ApplicationController
 
   def calculate_product_qty_lw
     calculator = Dentaku::Calculator.new
-    area = Measurement.square_meter(JSON.parse params[:areas_ids])
+    area = Measurement.square_meter(params[:areas])
     product = Product.find(params[:product_id])
     formula = product.calculation_formula
     qty = calculator.evaluate(formula.formula, area: area, area_covered: product.area_covered).to_f

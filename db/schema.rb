458effb076d37ba77833d56f5bf6c1d057e19034
João Victor <jovictor.miguel@gmail.com>
2020-07-21 23:09:50 -0300

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -195,6 +195,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_141440) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "square_feet", precision: 10, scale: 2
+    t.json "measures"
     t.index ["measurement_area_id"], name: "index_measurements_on_measurement_area_id"
   end
 

a8b66539bfc5de88868d654c1cdb113a7c9341aa
KleberDevMen <klebersubcontas@gmail.com>
2020-08-06 20:39:18 -0300

    required asterisk

---
+++ config/initializers/required_asterisk.rb
@@ -0,0 +1,27 @@
+module ActionView
+  module Helpers
+    class FormBuilder
+      include TagHelper
+
+      alias actual_label label
+
+      def label method, content_or_options = nil, options = nil, &block
+        if content_or_options && content_or_options.class == Hash
+          options = content_or_options
+        else
+          content = content_or_options
+        end
+        if object.present? and object.class != Ransack::Search
+          if object.class.validators_on(method).map(&:class).include? ActiveRecord::Validations::PresenceValidator
+            if options.class != Hash
+              options = {class: " required"}
+            else
+              options[:class] = ((options[:class] || "") + " required").split.uniq
+            end
+          end
+        end
+        actual_label(method, content, options || {}, &block)
+      end
+    end
+  end
+end

b7f3fd79e1fe7f53395014841a918c7d0d50a1e3
Lucas Cunha <lucascunha.dev@gmail.com>
2020-08-12 09:59:19 -0300

    fix customer problem

--- config/initializers/required_asterisk.rb
+++ config/initializers/required_asterisk.rb
@@ -6,21 +6,24 @@ module ActionView
       alias actual_label label
 
       def label method, content_or_options = nil, options = nil, &block
-        if content_or_options && content_or_options.class == Hash
-          options = content_or_options
-        else
-          content = content_or_options
-        end
-        if object.present? and object.class != Ransack::Search
-          if object.class.validators_on(method).map(&:class).include? ActiveRecord::Validations::PresenceValidator
-            if options.class != Hash
-              options = {class: " required"}
-            else
-              options[:class] = ((options[:class] || "") + " required").split.uniq
+        begin
+          if content_or_options && content_or_options.class == Hash
+            options = content_or_options
+          else
+            content = content_or_options
+          end
+          if object.present? and object.class != Ransack::Search
+            if object.class.validators_on(method).map(&:class).include? ActiveRecord::Validations::PresenceValidator
+              if options.class != Hash
+                options = {class: " required"}
+              else
+                options[:class] = ((options[:class] || "") + " required").split.uniq
+              end
             end
           end
+          actual_label(method, content, options || {}, &block)
         end
-        actual_label(method, content, options || {}, &block)
+      rescue
       end
     end
   end

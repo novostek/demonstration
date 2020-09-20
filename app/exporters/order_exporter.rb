df3965f266bdcaa2b6afdb64a2d1398344085495
KleberDevMen <klebersubcontas@gmail.com>
2020-09-19 21:34:18 -0300

    fix: order_exporter

--- app/exporters/order_exporter.rb
+++ app/exporters/order_exporter.rb
@@ -14,7 +14,7 @@ class OrderExporter < RailsExporter::Base
     column(:price) { |record| record.get_current_estimate.total }
     column(:received) { |record| record.transactions.where(status: :paid).sum(:value) }
     column(:profit) { |record| record.current_estimate.get_total_value - (record.total_cost || 0) }
-    column(:tax) { |record| ActionController::Base.helpers.number_to_currency(record.current_estimate.tax) + ' ' + (record.current_estimate.taxpayer.present? ? "(#{I18n.t "enumerize.estimate.taxpayer.#{record.current_estimate.taxpayer}"})" : '')  }
+    column(:tax) { |record| ActionController::Base.helpers.number_to_currency(record.current_estimate.tax || 0) + ' ' + (record.current_estimate.taxpayer.present? ? "(#{I18n.t "enumerize.estimate.taxpayer.#{record.current_estimate.taxpayer}"})" : '')  }
 
     column(:created_at) { |record| record.created_at.strftime("%m/%d/%Y %H:%M") }
     column(:updated_at) { |record| record.updated_at.strftime("%m/%d/%Y %H:%M") }
